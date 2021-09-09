f602fb2f6a83b74fb4647dbc86cc84224e388857
JackAngle156 <lehuythinh156@gmail.com>
2021-09-09 22:14:09 +0700

    First commit - Add comments & professional editing

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+###This source code is folked from Dapp university's source code: https://github.com/dappuniversity/blockchain_game
+
+## CHANGELOG
+ - Add comments which is very crucial to the source so that it can be easier to understand.
+
+ - And I changed img source to other images and fix the size of <img> to 30x30
+
+ - Re-define variables to be more professional ^-^
+
+ - For detail, please search 'XXX'
+
+ - The images is generated from https://github.com/HashLips/generative-art-opensource project
+
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,71 @@
+/* Main React File */
+
 import React, { Component } from 'react';
 import Web3 from 'web3'
 import './App.css';
 import MemoryToken from '../abis/MemoryToken.json'
 import brain from '../brain.png'
 
+// Variables for string manipulation
+// XXX Added these variables
+const baseURI = '/images/'
+const CARD_NAME = ['1', '2','3', '4','5', '6','7', '8', '9', '10',]
+const IMG_POSTFIX = '.png'
+
+
+// Array contains name and source of images
+// XXX Edit CARD_ARRAY
 const CARD_ARRAY = [
   {
-    name: 'fries',
-    img: '/images/fries.png'
+    name: CARD_NAME[0],
+    img: `${baseURI}${CARD_NAME[0]}${IMG_POSTFIX}`
   },
   {
-    name: 'cheeseburger',
-    img: '/images/cheeseburger.png'
+    name: CARD_NAME[0],
+    img: `${baseURI}${CARD_NAME[0]}${IMG_POSTFIX}`
   },
   {
-    name: 'ice-cream',
-    img: '/images/ice-cream.png'
+    name: CARD_NAME[3],
+    img: `${baseURI}${CARD_NAME[3]}${IMG_POSTFIX}`
   },
   {
-    name: 'pizza',
-    img: '/images/pizza.png'
+    name: CARD_NAME[3],
+    img: `${baseURI}${CARD_NAME[3]}${IMG_POSTFIX}`
   },
   {
-    name: 'milkshake',
-    img: '/images/milkshake.png'
+    name: CARD_NAME[9],
+    img: `${baseURI}${CARD_NAME[9]}${IMG_POSTFIX}`
   },
   {
-    name: 'hotdog',
-    img: '/images/hotdog.png'
+    name: CARD_NAME[9],
+    img: `${baseURI}${CARD_NAME[9]}${IMG_POSTFIX}`
   },
   {
-    name: 'fries',
-    img: '/images/fries.png'
+    name: CARD_NAME[6],
+    img: `${baseURI}${CARD_NAME[6]}${IMG_POSTFIX}`
   },
   {
-    name: 'cheeseburger',
-    img: '/images/cheeseburger.png'
+    name: CARD_NAME[6],
+    img: `${baseURI}${CARD_NAME[6]}${IMG_POSTFIX}`
   },
   {
-    name: 'ice-cream',
-    img: '/images/ice-cream.png'
+    name: CARD_NAME[1],
+    img: `${baseURI}${CARD_NAME[1]}${IMG_POSTFIX}`
   },
   {
-    name: 'pizza',
-    img: '/images/pizza.png'
+    name: CARD_NAME[1],
+    img: `${baseURI}${CARD_NAME[1]}${IMG_POSTFIX}`
   },
   {
-    name: 'milkshake',
-    img: '/images/milkshake.png'
+    name: CARD_NAME[5],
+    img: `${baseURI}${CARD_NAME[5]}${IMG_POSTFIX}`
   },
   {
-    name: 'hotdog',
-    img: '/images/hotdog.png'
+    name: CARD_NAME[5],
+    img: `${baseURI}${CARD_NAME[5]}${IMG_POSTFIX}`
   }
 ]
+
 
 class App extends Component {
 
@@ -63,6 +75,9 @@ class App extends Component {
     this.setState({ cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random()) })
   }
 
+  /* Function: loadWeb3()
+  Link metamask to the frontend
+  Parameters: NULL*/
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
@@ -76,6 +91,10 @@ class App extends Component {
     }
   }
 
+
+  /* Function: loadBlockchainData()
+  Load data from linked metamask wallet to the frontend
+  Parameters: NULL*/
   async loadBlockchainData() {
     const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
@@ -91,7 +110,7 @@ class App extends Component {
       this.setState({ token })
       const totalSupply = await token.methods.totalSupply().call()
       this.setState({ totalSupply })
-      // Load Tokens
+      // Load NFT token list
       let balanceOf = await token.methods.balanceOf(accounts[0]).call()
       for (let i = 0; i < balanceOf; i++) {
         let id = await token.methods.tokenOfOwnerByIndex(accounts[0], i).call()
@@ -105,18 +124,31 @@ class App extends Component {
     }
   }
 
+
+  /* Function: chooseImage
+  Load data from linked metamask wallet to the frontend
+  Parameters: 
+    any - cardId  -  id of the card you're choosing
+    */
   chooseImage = (cardId) => {
     cardId = cardId.toString()
-    if(this.state.cardsWon.includes(cardId)) {
+
+    if(this.state.cardsWon.includes(cardId)) {// If found dupplicates
       return window.location.origin + '/images/white.png'
     }
     else if(this.state.cardsChosenId.includes(cardId)) {
       return CARD_ARRAY[cardId].img
     } else {
-      return window.location.origin + '/images/blank.png'
+      return window.location.origin + '/images/penguin.png'
     }
   }
 
+
+  /* Function: flipCard
+  Flip the card
+  Parameters: 
+    any - cardId  -  id of the card you're choosing
+    */
   flipCard = async (cardId) => {
     let alreadyChosen = this.state.cardsChosen.length
 
@@ -131,25 +163,34 @@ class App extends Component {
   }
 
 
+  /* Function: checkForMatch
+  Check for matching
+  Case: 
+    1 - Match: perform minting NFT token
+    2 - No match: "Sorry, try again"
+    */
   checkForMatch = async () => {
     const optionOneId = this.state.cardsChosenId[0]
     const optionTwoId = this.state.cardsChosenId[1]
 
-    if(optionOneId == optionTwoId) {
+    if(optionOneId === optionTwoId) {
       alert('You have clicked the same image!')
     } else if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
       alert('You found a match')
+
+      /*MINT function of the NFT token*/
       this.state.token.methods.mint(
         this.state.account,
         window.location.origin + CARD_ARRAY[optionOneId].img.toString()
       )
-      .send({ from: this.state.account })
+      .send({ from: this.state.account })//Senf from this wallet
       .on('transactionHash', (hash) => {
         this.setState({
           cardsWon: [...this.state.cardsWon, optionOneId, optionTwoId],
           tokenURIs: [...this.state.tokenURIs, CARD_ARRAY[optionOneId].img]
         })
-      })
+      }) //Callback when receive transactionHash
+
     } else {
       alert('Sorry, try again')
     }
@@ -186,6 +227,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+          {/* XXX  Fix the size og <img> element*/}
           <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" />
           &nbsp; Memory Tokens
           </a>
@@ -209,6 +251,8 @@ class App extends Component {
                         key={key}
                         src={this.chooseImage(key)}
                         data-id={key}
+                        width="100" 
+                        height="100"
                         onClick={(event) => {
                           let cardId = event.target.getAttribute('data-id')
                           if(!this.state.cardsWon.includes(cardId.toString())) {
@@ -218,8 +262,6 @@ class App extends Component {
                       />
                     )
                   })}
-
-
                 </div>
 
                 <div>
@@ -231,6 +273,8 @@ class App extends Component {
                     { this.state.tokenURIs.map((tokenURI, key) => {
                       return(
                         <img
+                          width="100" 
+                          height="100"
                           key={key}
                           src={tokenURI}
                         />
